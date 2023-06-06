ef488c70c36e784bcc4811ccb4f6a5adac0e5749
sergmzk <sergmzk@gmail.com>
2023-06-06 19:00:18 +0700

    minor changes

--- rainbow/settings.py
+++ rainbow/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / 'static', )
 if not DEBUG:
-    STATIC_ROOT = '/home/rainbowmzk/rainbow/static'
+    STATIC_ROOT = '/home/serjmzk/rainbow/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
